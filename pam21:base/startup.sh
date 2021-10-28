260ca33d32bdc6d0bcb73a8bcdfabb1f519f3951
isx36579183 <isx36579183@i11.informatica.escoladeltreball.org>
2021-10-28 13:56:10 +0200

    28.10.21 - 1st class PAM

--- pam21:base/startup.sh
+++ pam21:base/startup.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+wq#! /bin/bash
 
-useradd unix01
-useradd unix02
-useradd unix03
-echo -e "unix01\nunix01" | passwd --stdin unix01 # Posa la contrasenya
-echo -e "unix02\nunix02" | passwd --stdin unix02 # Posa la contrasenya
-echo -e "unix03\nunix03" | passwd --stdin unix03 # Posa la contrasenya
+useradd -m -s /bin/bash unix01 # -m crea directorio y -s shell
+useradd -m -s /bin/bash unix02
+useradd -m -s /bin/bash unix03
+echo -e "unix01\nunix01" | passwd unix01 # Posa la contrasenya
+echo -e "unix02\nunix02" | passwd unix02 # Posa la contrasenya
+echo -e "unix03\nunix03" | passwd unix03 # Posa la contrasenya # sense --stdin por debian echo -e por el control
 
 cp /opt/docker/login.defs /etc/login.defs
 
