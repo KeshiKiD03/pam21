c72ea52de8f946c4c3709f2d2112bdcf1e9fbb8c
Your Name <you@example.com>
2021-11-11 23:06:54 +0100

    11.11.21 - Python

--- pam21:python/pam_nologin.py
+++ pam21:python/pam_nologin.py
@@ -1,29 +1,102 @@
 #
-# Duplicates pam_permit.c
+#Emulatewhatpam_nologin.cdoes. 
+#
+import pwd 
+#
+# Parse our command line. 
+#
+def parse_args(pamh, argv): 
+#
+# Parse the arguments. 
+#
+nologin_file = "/etc/nologin" 
+retval_when_nofile = pamh.PAM_IGNORE 
+forarg inargv[1:]: 
+	if arg.starts_with("file="): 
+	nologin_file = arg[5:] 
+	elif arg == "successok": 
+	retval_when_nofile = pamh.PAM_SUCCESS 
+returnnologin_file, retval_when_nofile 
+
+#
+#Checkthe/etc/nologinfile. 
+#
+
+def check_nologin(pamh, nologin_file, retval_when_nofile): 
+
+#
+# Get the user name. 
+#
+try: 
+	username = pamh.get_user() 
+except pamh.exception: 
+	username = None 
+if username == None: 
+	return pamh.PAM_USER_UNKNOWN 
+#
+# Can we open the file? 
+#
+
+try:
+	handle = file(nologin_file, "r") 
+except EnvironmentError: 
+	return retval_when_nofile 
+
 # 
-DEFAULT_USER = "nobody"
+# Print the message. 
+# 
 
-def pam_sm_authenticate(pamh, flags, argv):
-    try:
-        user = pamh.get_user(None)
-    except pamh.exception, e:
-        return e.pam_result
+try: 
+	try: 
+		msg = handle.read() 
+		except EnvironmentError: 
+		return pamh.PAM_SYSTEM_ERR 
+finally: 
 
-    if user == None:
-        pamh.user = DEFAULT_USER
-    return pamh.PAM_SUCCESS
+	handle.close() 
+	
+# 
+# Read the user's password entry so we can check if he is root. 
+# Root can login regardless. 
+# 
 
-def pam_sm_setcred(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+try: 
+	pwent = pwd.getpwnam(username) 
+except KeyError: 
+	retval = pamh.PAM_USER_UNKNOWN 
+	msg_style = pamh.PAM_ERROR_MSG 
 
-def pam_sm_acct_mgmt(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+else: 
+	if pwent[2] == 0: # Is this root? 
+	retval = pamh.PAM_SUCCESS 
+	msg_style = pamh.PAM_TEXT_INFO 
+	else: 
+	retval = pamh.PAM_AUTH_ERR 
+	msg_style = pamh.PAM_ERROR_MSG 
 
-def pam_sm_open_session(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+# 
+# Display the message 
+# 
 
-def pam_sm_close_session(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+try: 
+	pamh.conversation(pamh.Message(msg_style, msg)) 
+except pamh.exception: 
+	return pamh.PAM_SYSTEM_ERR 
+return retval 
 
-def pam_sm_chauthtok(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
+# 
+# Entry points we handle. 
+# 
+
+def pam_sm_authenticate(pamh, flags, argv): 
+	nologin_file, retval_when_nofile = parse_args(pamh, argv) 
+	return check_nologin(pamh, nologin_file, retval_when_nofile) 
+
+def pam_sm_setcred(pamh, flags, argv): 
+	nologin_file, retval_when_nofile = parse_args(pamh, argv) 
+	return retval_when_nofile 
+
+def pam_sm_acct_mgmt(pamh, flags, argv): 
+	nologin_file, retval_when_nofile = parse_args(pamh, argv) 
+	return check_nologin(pamh, nologin_file, retval_when_nofile)
+
--- pam21:python/startup.sh
+++ pam21:python/startup.sh
@@ -13,22 +13,7 @@ echo -e "unix03\nunix03\n" | passwd unix03
 # Copiar arxius necesaris
 cp /opt/docker/ldap.conf /etc/ldap/ldap.conf
 cp /opt/docker/login.defs /etc/login.defs
-cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
-cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
-cp /opt/docker/common-auth /etc/pam.d/common-auth
-cp /opt/docker/common-account /etc/pam.d/common-account
-cp /opt/docker/common-password /etc/pam.d/common-password
-cp /opt/docker/common-session /etc/pam.d/common-session
-
-
-# Engegar dimonis necesaris
-/usr/sbin/nscd
-/usr/sbin/nslcd
-
-# Asegurar que funcione
-getent passwd
-getent group
 
 # Interactiu
 /bin/bash
